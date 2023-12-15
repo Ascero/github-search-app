7b38d336e49222a84efa36607b3426f29309b529
Daniel Vieira <danvie11@gmail.com>
2023-12-15 19:13:44 +0000

    GSA-0002: install MUI and config lint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,16 +3,19 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
   ],
+  plugins: ['react', 'react-hooks', 'jsx-a11y', 'prettier', 'react-refresh'],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
   rules: {
+    "react/react-in-jsx-scope": "off",
+    'prettier/prettier': 'error',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
   },
-}
+};
