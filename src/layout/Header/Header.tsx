b626ee00e41d5b982d9d96339a9692b4f51cf180
Daniel Vieira <danvie11@gmail.com>
2023-12-18 19:52:05 +0000

    GSA-0017: add navigation in header

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import styles from './Header.module.css';
 import reactLogo from '../../assets/react.svg';
+import { NavLink, useLocation } from 'react-router-dom';
+import { Button } from '@mui/material';
 
-const Header: React.FC<{ children?: React.ReactNode }> = (props) => {
+const Header: React.FC = () => {
+  const location = useLocation();
+
   return (
     <div className={styles.header}>
       <div className={styles.logo}>
-        <img src={reactLogo} alt="App logo" />
+        <NavLink to="/">
+          <img src={reactLogo} alt="App logo" />
+        </NavLink>
       </div>
-      {props.children}
+      {location.pathname === '/' ? (
+        <NavLink to="/favorites">
+          <Button variant="contained" size="small">
+            My Favorites
+          </Button>
+        </NavLink>
+      ) : (
+        <NavLink to="/">
+          <Button variant="contained" size="small">
+            Back to search
+          </Button>
+        </NavLink>
+      )}
     </div>
   );
 };
